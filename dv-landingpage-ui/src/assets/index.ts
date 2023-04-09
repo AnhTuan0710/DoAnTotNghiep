c7f2de9050339e0baf5d8047268143ffb4a5bac4
Your Name <you@example.com>
2023-04-09 20:35:30 +0700

    fix conflict

--- dv-landingpage-ui/src/assets/index.ts
+++ dv-landingpage-ui/src/assets/index.ts
@@ -1,7 +1,6 @@
 export const ICON_USER = require("./images/user.png");
 export const ICON_DRIVER = require("./images/driver.png");
 export const ICON_CALENDAR = require("./images/calendar.png");
-<<<<<<< HEAD
 export const SLIDE_1 = require("./images/slide1.jpg");
 export const SLIDE_2 = require("./images/slide2.png");
 export const SLIDE_4 = require("./images/slide4.jpg");
@@ -12,6 +11,4 @@ export const BRANCH_3 = require("./images/branch3.jpg");
 export const BRANCH_4 = require("./images/branch4.jpg");
 export const BRANCH_5 = require("./images/branch5.jpg");
 export const BRANCH_6 = require("./images/branch6.jpg");
-=======
 export const BANNER_MAY1 = require("./images/banner-may1.png");
->>>>>>> 8b91beb690538cd2a2e4106c3a02dae7d064c17e
